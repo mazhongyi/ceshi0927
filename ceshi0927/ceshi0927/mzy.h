0f3f3b78a1225a453e664a944fa25734e36f109b
mazhongyi2 <1013751425@qq.com>
2017-09-27 13:28:19 +0800

    @interface mzy : NSObject

---
+++ ceshi0927/ceshi0927/mzy.h
@@ -0,0 +1,13 @@
+//
+//  mzy.h
+//  ceshi0927
+//
+//  Created by iSA-Test on 2017/9/27.
+//  Copyright © 2017年 iSA-Test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mzy : NSObject
+
+@end
---
+++ ceshi0927/ceshi0927/mzy.m
@@ -0,0 +1,13 @@
+//
+//  mzy.m
+//  ceshi0927
+//
+//  Created by iSA-Test on 2017/9/27.
+//  Copyright © 2017年 iSA-Test. All rights reserved.
+//
+
+#import "mzy.h"
+
+@implementation mzy
+
+@end
