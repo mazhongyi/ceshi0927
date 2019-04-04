42a14bde510cf5cee9d090b3e92ba2c019ce76ee
mazhongyi2 <>
2019-04-04 14:23:28 +0800

    Revert "Merge branch 'dev_0920_mzy' into dev_0921_mzy"
    This reverts commit ebe304dcbf53aa4ff1d07a577734e917245d2efd.

---
+++ ceshi0927/ceshi0927/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi0927
+//
+//  Created by iSA-Test on 2017/9/27.
+//  Copyright © 2017年 iSA-Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ceshi0927/ceshi0927Tests/ceshi0927Tests.m
@@ -0,0 +1,39 @@
+//
+//  ceshi0927Tests.m
+//  ceshi0927Tests
+//
+//  Created by iSA-Test on 2017/9/27.
+//  Copyright © 2017年 iSA-Test. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ceshi0927Tests : XCTestCase
+
+@end
+
+@implementation ceshi0927Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
